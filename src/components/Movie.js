32e6beb02150c6e39e091d923592738f2a610298
yoona <jinalks43@gmail.com>
2023-12-09 20:13:11 +0900

    add summary length limit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,7 +8,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
             <h2>
                 <Link to={`/movie/${id}`}>{title}</Link>
             </h2>
-            <p>{summary}</p>
+            <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
             <ul>
                 {genres.map(g =>
                     <li key={g}>{g}</li>
@@ -19,7 +19,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
-    id:PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
     coverImg: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
